59fe7732d2fea21d9d1ff25e84805f6df54bab95
Daniel <daanielvb@gmail.com>
2016-10-24 20:42:36 -0200

    criando classes das telas internas

--- app/src/main/java/com/example/daniel/fitkeeper/HomeActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.example.daniel.fitkeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
     private Button homeBtn;
     private Button workoutBtn;
     private Button subscriptionBtn;
@@ -17,6 +19,28 @@ public class HomeActivity extends AppCompatActivity {
         homeBtn = (Button) findViewById(R.id.btnHome);
         workoutBtn = (Button) findViewById(R.id.btnWorkout);
         subscriptionBtn = (Button) findViewById(R.id.btnSubscription);
-
+        homeBtn.setOnClickListener(this);
+        workoutBtn.setOnClickListener(this);
+        subscriptionBtn.setOnClickListener(this);
     }
+
+
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnHome:
+                Intent it = new Intent(HomeActivity.this,InfoActivity.class);
+                startActivity(it);
+                break;
+            case R.id.btnWorkout:
+                Intent it2 = new Intent(HomeActivity.this,WorkoutActivity.class);
+                startActivity(it2);
+                break;
+            case R.id.btnSubscription:
+                Intent it3 = new Intent(HomeActivity.this,SubscriptionActivity.class);
+                startActivity(it3);
+                break;
+        }
+    }
+
+
 }
