d45b03c84c6eed8f47d6cee1258e936aa725dc65
Daniel <daanielvb@gmail.com>
2016-10-24 21:11:10 -0200

    spinner com conteudo fixo

--- app/src/main/java/com/example/daniel/fitkeeper/InfoActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/InfoActivity.java
@@ -2,6 +2,11 @@ package com.example.daniel.fitkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class InfoActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        List<String> spinnerArray =  new ArrayList<String>();
+       //spinnerArray.addAll(getResources().getStringArray(R.id.months));
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner);
+        sItems.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/daniel/fitkeeper/Person.java
@@ -0,0 +1,18 @@
+package com.example.daniel.fitkeeper;
+
+import java.util.jar.Attributes;
+
+/**
+ * Created by Daniel on 24/10/2016.
+ */
+public class Person {
+    public String name;
+    public int age;
+    public float height;
+    public float weight;
+    public  float imc;
+
+
+
+
+}
