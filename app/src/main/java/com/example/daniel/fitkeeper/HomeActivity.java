ea2831d9be83f444fbbd0d09b55aa8145d722268
Daniel <daanielvb@gmail.com>
2016-10-24 20:28:00 -0200

    botoes da home

--- app/src/main/java/com/example/daniel/fitkeeper/HomeActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/HomeActivity.java
@@ -2,12 +2,21 @@ package com.example.daniel.fitkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button homeBtn;
+    private Button workoutBtn;
+    private Button subscriptionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        homeBtn = (Button) findViewById(R.id.btnHome);
+        workoutBtn = (Button) findViewById(R.id.btnWorkout);
+        subscriptionBtn = (Button) findViewById(R.id.btnSubscription);
+
     }
 }
