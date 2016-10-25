70ec4994d65284d3e62b312bedd858a6b019908c
Daniel <daanielvb@gmail.com>
2016-10-24 23:00:26 -0200

    salvando campos de texto ao rotacionar na home e na info activity

--- app/src/main/java/com/example/daniel/fitkeeper/InfoActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/InfoActivity.java
@@ -4,11 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InfoActivity extends AppCompatActivity {
+    public String name;
+    public String age;
+    public String height;
+    public String weight;
+    public  String imc;
+
+    private TextView nameText;
+    private TextView ageText;
+    private TextView heightText;
+    private TextView weightText;
+    private TextView imcText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +28,18 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        if (savedInstanceState != null) {
+            name = savedInstanceState.getString("name");
+            age = savedInstanceState.getString("age");
+            height = savedInstanceState.getString("height");
+            weight = savedInstanceState.getString("weight");
+            imc = savedInstanceState.getString("imc");
+            atualizaDados();
+        }
+
         List<String> spinnerArray =  new ArrayList<String>();
        //spinnerArray.addAll(getResources().getStringArray(R.id.months));
-        
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, spinnerArray);
 
@@ -26,5 +47,36 @@ public class InfoActivity extends AppCompatActivity {
         Spinner sItems = (Spinner) findViewById(R.id.spinner);
         sItems.setAdapter(adapter);
 
+    }
+
+    private void atualizaDados() {
+        if (name != null)
+            nameText.setText(name);
+        if(age != null)
+            ageText.setText(age);
+        if(height != null)
+            heightText.setText(height);
+        if (weight != null)
+            weightText.setText(weight);
+        if(imc != null)
+            imcText.setText(imc);
+    }
+
+    private void salvaDados(){
+        name = nameText.getText().toString();
+        age = ageText.getText().toString();
+        height = heightText.getText().toString();
+        weight = weightText.getText().toString();
+        imc = imcText.getText().toString();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("name", name);
+        outState.putString("age", age);
+        outState.putString("weight", weight);
+        outState.putString("height", height);
+        outState.putString("imc", imc);
     }
 }
--- app/src/main/java/com/example/daniel/fitkeeper/LoginActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.daniel.fitkeeper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,22 +11,48 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
     private Button loginBtn;
     private TextView emailText;
-    private TextView password;
+    private TextView passwordtText;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        if (savedInstanceState != null) {
+            email = savedInstanceState.getString("email");
+            password = savedInstanceState.getString("password");
+            atualizaDados();
+        }
         loginBtn = (Button) findViewById(R.id.loginBtn);
         emailText = (TextView) findViewById(R.id.editTextMail);
-        password = (TextView) findViewById(R.id.editTextPassword);
+        passwordtText = (TextView) findViewById(R.id.editTextPassword);
         loginBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                login(emailText.getText().toString(),password.getText().toString());
+                salvaDados();
+                login(email,password);
             }
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("email", email);
+        outState.putString("password", password);
+    }
+
+    private void atualizaDados() {
+        if (email != null)
+            emailText.setText(email);
+        if(password != null)
+            passwordtText.setText(password);
+    }
+
+    private void salvaDados(){
+        email = emailText.getText().toString();
+        password = passwordtText.getText().toString();
+    }
 
     public void login(String email, String password){
             if(!email.equals("") && !password.equals("")){
