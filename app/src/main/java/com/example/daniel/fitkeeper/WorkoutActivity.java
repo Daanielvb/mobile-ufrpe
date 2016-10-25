6c45b46bcb71bde4c86c22d68b242773e52ed7e2
Daniel <daanielvb@gmail.com>
2016-10-25 14:49:41 -0200

    spinner com dias da semana

--- app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
@@ -2,12 +2,31 @@ package com.example.daniel.fitkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WorkoutActivity extends AppCompatActivity {
+    private String[] weeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+
+        List<String> spinnerArray =  new ArrayList<String>();
+
+        weeks = getResources().getStringArray(R.array.week);
+        spinnerArray.addAll(Arrays.asList(weeks));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.snipperWeek);
+        sItems.setAdapter(adapter);
     }
 }
