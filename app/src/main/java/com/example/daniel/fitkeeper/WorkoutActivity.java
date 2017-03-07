ad9a16d0d85f89b78178d33e92524b3f84bd55f1
daniel vilas-boas <daanielvb@gmail.com>
2017-03-07 20:44:10 -0300

    check dos steps do treino

--- app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
@@ -8,8 +8,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -20,12 +23,15 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 
-public class WorkoutActivity extends AppCompatActivity implements View.OnClickListener{
+public class WorkoutActivity extends AppCompatActivity implements View.OnClickListener {
     private String[] days;
     private ImageButton backBtn;
+    private Button finishWorkoutBtn;
     private TextView currentDayText;
     SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
     Calendar calendar = Calendar.getInstance();
+    CheckBox c1, c2, c3, c4;
+    public static int checkCount = 0;
 
 
     @Override
@@ -34,7 +40,7 @@ public class WorkoutActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_workout);
         setUI();
 
-        List<String> spinnerArray =  new ArrayList<String>();
+        List<String> spinnerArray = new ArrayList<String>();
 
         days = getResources().getStringArray(R.array.week);
         spinnerArray.addAll(Arrays.asList(days));
@@ -59,27 +65,81 @@ public class WorkoutActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.backBtn:
                 finish();
                 break;
         }
     }
 
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.checkbox_1:
+                if (checked)
+                    setRowColor(R.id.tablerow1, true);
+                else
+                    setRowColor(R.id.tablerow1, false);
+                break;
+            case R.id.checkbox_2:
+                if (checked)
+                    setRowColor(R.id.tablerow2, true);
+                else
+                    setRowColor(R.id.tablerow2, false);
+                break;
+            case R.id.checkbox_3:
+                if (checked)
+                    setRowColor(R.id.tablerow3, true);
+                else
+                    setRowColor(R.id.tablerow3, false);
+                break;
+            case R.id.checkbox_4:
+                if (checked)
+                    setRowColor(R.id.tablerow4, true);
+                else
+                    setRowColor(R.id.tablerow4, false);
+                break;
+        }
+    }
+
+    private void setRowColor(int id, boolean flag) {
+        TableRow tr = (TableRow) findViewById(id);
+        if (flag) {
+            checkCount++;
+            tr.setBackgroundColor(getResources().getColor(R.color.lightGray));
+        }
+        else {
+            tr.setBackgroundColor(getResources().getColor(R.color.white));
+            checkCount--;
+        }
+        releaseEndWorkoutButton();
+    }
+
+    private void releaseEndWorkoutButton() {
+        if(checkCount > 3)
+            finishWorkoutBtn.setVisibility(View.VISIBLE);
+        else
+            finishWorkoutBtn.setVisibility(View.GONE);
+    }
+
     public void setUI() {
         currentDayText = (TextView) findViewById(R.id.currentDayTxt);
         backBtn = (ImageButton) findViewById(R.id.backBtn);
+        finishWorkoutBtn = (Button) findViewById(R.id.finish_workout_btn);
         backBtn.setOnClickListener(this);
+        c1 = (CheckBox) findViewById(R.id.checkbox_1);
+        c2 = (CheckBox) findViewById(R.id.checkbox_2);
+        c3 = (CheckBox) findViewById(R.id.checkbox_3);
+        c4 = (CheckBox) findViewById(R.id.checkbox_4);
     }
 
-    public String getCurrentDay(){
+    public String getCurrentDay() {
         return dayFormat.format(calendar.getTime());
     }
 
-    public int getIndexByName(String day){
-        String preparedDay = day.replaceAll("f","F");
+    public int getIndexByName(String day) {
+        String preparedDay = day.replaceAll("f", "F");
         preparedDay = preparedDay.substring(0, 1).toUpperCase() + preparedDay.substring(1);
-        Log.e("fitkeeper", preparedDay);
         return Arrays.asList(days).indexOf(preparedDay);
     }
 
