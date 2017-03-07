4e6a7c719905ac35e29b8598efb0660008cf28e6
daniel vilas-boas <daanielvb@gmail.com>
2017-03-07 19:30:09 -0300

    abrir workout no dia atual da semana internacionalizado

--- app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
+++ app/src/main/java/com/example/daniel/fitkeeper/WorkoutActivity.java
@@ -1,6 +1,7 @@
 package com.example.daniel.fitkeeper;
 
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +12,21 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
 
 public class WorkoutActivity extends AppCompatActivity implements View.OnClickListener{
     private String[] days;
     private ImageButton backBtn;
     private TextView currentDayText;
+    SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+    Calendar calendar = Calendar.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,7 @@ public class WorkoutActivity extends AppCompatActivity implements View.OnClickLi
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spinnerWeek);
         spinner.setAdapter(adapter);
+        spinner.setSelection(getIndexByName(getCurrentDay()));
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
@@ -62,4 +71,16 @@ public class WorkoutActivity extends AppCompatActivity implements View.OnClickLi
         backBtn = (ImageButton) findViewById(R.id.backBtn);
         backBtn.setOnClickListener(this);
     }
+
+    public String getCurrentDay(){
+        return dayFormat.format(calendar.getTime());
+    }
+
+    public int getIndexByName(String day){
+        String preparedDay = day.replaceAll("f","F");
+        preparedDay = preparedDay.substring(0, 1).toUpperCase() + preparedDay.substring(1);
+        Log.e("fitkeeper", preparedDay);
+        return Arrays.asList(days).indexOf(preparedDay);
+    }
+
 }
